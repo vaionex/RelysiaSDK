e59f2c38c36d64e61e317d36480f713a28515f8a
adityapanther <raj68518@gmail.com>
2022-11-04 14:26:44 +0530

    missing transaction class added & version changed

--- src/relysia-sdk.js
+++ src/relysia-sdk.js
@@ -7,19 +7,20 @@ const Paymail = require('./paymail');
 const Notifications = require('./notifications');
 const Delete = require('./delete');
 const Contracts = require('./contracts');
+const Transaction = require('./transactions/index');
 
 class RelysiaSDK {
   constructor(config) {
     this.authentication = new Authentication(config);
-    this.user = new User(this.authentication.auth);
-    this.wallet = new Wallet(this.authentication.auth);
-    this.feeManager = new FeeManager(this.authentication.auth);
-    this.utility = new Utility(this.authentication.auth);
-    this.paymail = new Paymail(this.authentication.auth);
-    this.notifications = new Notifications(this.authentication.auth);
-    this.delete = new Delete(this.authentication.auth);
-    this.paymail = new Paymail(this.authentication.auth);
     this.contracts = new Contracts(this.authentication.auth);
+    this.delete = new Delete(this.authentication.auth);
+    this.feeManager = new FeeManager(this.authentication.auth);
+    this.notification = new Notifications(this.authentication.auth);
+    this.paymail = new Paymail(this.authentication.auth);
+    this.transaction = new Transaction(this.authentication.auth),
+    this.user = new User(this.authentication.auth);
+    this.utility = new Utility(this.authentication.auth);
+    this.wallet = new Wallet(this.authentication.auth);
   }
 }
 
